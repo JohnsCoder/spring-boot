145fd48afd644181ddf59bd53497382d5682a9e4
JohnsCoder <joaopdossantos1515@gmail.com>
2023-07-11 15:38:13 -0300

    feat: implement findAll and findbyId endpoints and services

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -1,20 +1,30 @@
 package com.educandoweb.course.controllers;
 
 import com.educandoweb.course.entities.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-public User findAll() {
-    User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "1234567");
-    return user;
-}
+    public List<User> findAll() {
+        List<User> list = userService.findAll();
+        return list;
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return user;
+    }
 
 
 }
